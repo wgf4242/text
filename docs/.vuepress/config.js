9795e50863054bfba61feb1c692cd87ed2a78216
wgf4242 <wgf4242@gmail.com>
2020-06-24 22:00:29 +0800

    Init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+// .vuepress/config.js
+
+module.exports = {
+  theme: 'reco',
+  title: 'wgf4242\'s Blog',
+  description: 'Just playing around',
+  markdown: {
+    lineNumbers: true
+  },
+  themeConfig: {
+  	displayAllHeaders: true, // 默认值：false
+    sidebar: 'auto'
+  }
+}  
---
+++ docs/README.md
@@ -0,0 +1,5 @@
+# h1
+
+## h2
+
+'# Hello VuePress!' 1234
