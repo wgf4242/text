f3bd95ed8d0b629e76eebf325ca3a04b6def4507
wgf4242 <wgf4242@gmail.com>
2021-07-29 00:07:48 +0800

    sync

--- docs/ctf/scripts/Crypto_hextostring_修错.py
+++ docs/ctf/scripts/Crypto_hextostring_修错.py
@@ -5,3 +5,10 @@ from Crypto.Util.number import *
 print(long_to_bytes(0x65a6d78685a33733059584e6b4e575266613346704d5770754d563878614446696233303d))
 
 # print(bytes.fromhex('5a6d78685a33733059584e6b4e575266613346704d5770754d563878614446696233303d'))
+
+from Crypto.Hash import MD5
+
+
+h = MD5.new()
+h.update(b'savedInstanceState')
+print(h.hexdigest())
