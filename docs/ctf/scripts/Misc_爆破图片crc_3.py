9a1cf6f66c8cf88046bf49d68d9356bb031ca784
wgf4242 <wgf4242@gmail.com>
2021-10-13 06:32:38 +0800

    sync

--- docs/ctf/scripts/Misc_爆破图片crc_3.py
+++ docs/ctf/scripts/Misc_爆破图片crc_3.py
@@ -2,9 +2,11 @@
 import binascii
 import struct
 
-crc32key = 0xC61C9FAA
+f = open('1.png', 'rb').read()
 
-f = open('misc4.png', 'rb').read()
+crc_buffer = f[0x1d:0x21]
+crc32key = struct.unpack('>I', crc_buffer)[0]
+
 for i in range(0, 65535):
     height = struct.pack('>i', i)
     data = f[0xc:0x14] + height + b'\x08\x06\x00\x00\x00'
