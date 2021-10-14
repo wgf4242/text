1464b6cdb56449c1f92d33723e9f2e2ea2d4627b
wgf4242 <wgf4242@gmail.com>
2021-10-14 18:11:43 +0800

    sync

---
+++ docs/ctf/scripts/misc_word_frequency.py
@@ -0,0 +1,21 @@
+import re
+from collections import Counter
+
+f = open('我flag呢？', 'r', encoding='utf8')
+a = ''
+b = ''
+for line in f.readlines():
+    s = re.search(r"\{(.*)\}", line)
+    res = s.groups()[0]
+    a +=res
+    b +=res.lower()
+counter = Counter(a)
+counterb = Counter(b)
+print(counter)
+print(counterb)
+for k,num in counter.most_common():
+    print(k, end='')
+print()
+for k,num in counterb.most_common():
+    print(k, end='')
+
