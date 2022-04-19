a47505f32a955000e65dc0d85486bb1d2c6908fd
wgf4242 <wgf4242@gmail.com>
2022-04-19 08:07:51 +0800

    sync

---
+++ docs/ctf/scripts/Draw_GetPixel_all.py
@@ -0,0 +1,29 @@
+# 每10像素读一次
+from PIL import Image
+
+pic = Image.open("aaaaa.png")  # type: Image.Image
+pic = pic.convert('RGB')
+width, height = pic.size
+lst = []
+for x in range(width):  # 放大图片看一格子是10x10像素
+    for y in range(height):
+        pixel = pic.getpixel((x, y))
+        if pixel == (255, 255, 255):
+            continue
+        r, g, b = pixel
+        # print(pixel)
+        lst.extend(pixel)
+from collections import Counter
+
+data = ''
+for i in lst:
+    if i == 0: continue
+    if i < 32 or i > 128:
+        continue
+    data += chr(i)
+
+print(data)
+
+counter = Counter(data)
+for k, count in counter.most_common():
+    print(k, end='')  # key就是前面的  theKEYis:#R@/&p~!
