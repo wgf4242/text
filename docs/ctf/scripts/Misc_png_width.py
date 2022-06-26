755a4507392d6a21057d67ddebd3ced5630e8e22
wgf4242 <wgf4242@gmail.com>
2022-06-27 00:18:13 +0800

    sync

---
+++ docs/ctf/scripts/Misc_png_width.py
@@ -0,0 +1,22 @@
+# CRC给的值有错误时，可能要宽度爆破。有时要宽高一起爆
+import binascii
+import os
+import struct
+
+from Crypto.Util import number
+
+p = open('flag.png', 'rb').read()
+# print(p[0x14:0x17]+chr(0xaf).encode()[-1:])
+count = 0
+height, = struct.unpack('>I', p[0x14:0x18])
+folder = 'te2/'
+if not os.path.exists(folder):
+    os.mkdir(folder)
+
+for width in range(1, 300):
+    data = p[:0x10] + width.to_bytes(4, 'big') + height.to_bytes(4, 'big') + p[0x18:0x1d]
+    p2 = data + number.long_to_bytes(binascii.crc32(data[0xc:0x1d]) & 0xffffffff) + p[0x21:]
+    p1 = open(folder + str(count) + '.png', 'wb')
+    count += 1
+    p1.write(p2)
+    p1.close()
