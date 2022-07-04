83d03aaa32fa33b91be91e7fad4ace7e62305eb9
wgf4242 <wgf4242@gmail.com>
2022-07-05 07:26:40 +0800

    sync

---
+++ docs/ctf/scripts/Misc_picture_QRCode_by_pillow_画二维码01coordinate2_by_pyplot_scatter.py
@@ -0,0 +1,24 @@
+"""
+@description: 画二维码,
+data format like
+0 0
+0 1
+0 2
+0 3
+0 4
+0 5
+0 6
+"""
+
+import matplotlib.pyplot as plt
+
+f = open("flag.txt", "r").readlines()
+
+x_li = []
+y_li = []
+for i in range(len(f)):
+    x_li.append(int(f[i].strip().split()[0]))
+    y_li.append(int(f[i].strip().split()[1]))
+
+plt.scatter(x_li, y_li)
+plt.show()
