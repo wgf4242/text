ea83169f8018696199ff2999266c21fc45ef98f3
wgf4242 <wgf4242@gmail.com>
2023-06-29 07:51:12 +0800

    sync

---
+++ docs/ctf/scripts/realworld/tools_get.sh
@@ -0,0 +1,16 @@
+function __get() {
+  read proto server path <<<$(echo ${1//// })
+  DOC=/${path// //}
+  HOST=${server//:*}
+  PORT=${server//*:}
+  [[ x"${HOST}" == x"${PORT}" ]] && PORT=80
+  exec 3<>/dev/tcp/${HOST}/$PORT
+  echo -en "GET ${DOC} HTTP/1.0\r\nHost: ${HOST}\r\n\r\n" >&3
+  (while read line; do
+   [[ "$line" == $'\r' ]] && break
+  done && cat) <&3
+  exec 3>&-
+}
+
+# https://mp.weixin.qq.com/s/ntjMPeqypK8W1xs287W1RQ
+# __get http://pm.myapp.com/xxxx.exe >1.exe
