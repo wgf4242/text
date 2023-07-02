2f41491f7e76a554d16b505905c93360482af99e
wgf4242_notebook <wgf4242@gmail.com>
2023-07-02 18:28:56 +0800

    sync

--- docs/ctf/scripts/realworld/blue_11_secure_mysql.sh
+++ docs/ctf/scripts/realworld/blue_11_secure_mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 user=root
-pass=AnyWhereis5@0
-
+pass=root
+secure_pass=AnyWhereis5@0
 # mysql -u$user -p$pass -e "DROP USER ''@'$(hostname)'"
 # 防写日志
 # revoke ALL on *.* from admin@'%';
@@ -13,14 +13,15 @@ pass=AnyWhereis5@0
 # --- Generate sql file ---
 tee secure_mysql.sql <<-EOF
 use mysql;
-CREATE USER 'root'@'%' IDENTIFIED BY 'AnyWhereis5@0';
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'AnyWhereis5@0';
+CREATE USER 'root'@'%' IDENTIFIED BY '$secure_pass';
+UPDATE mysql.user SET authentication_string = PASSWORD('$secure_pass') WHERE User = 'root';
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '$secure_pass';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
-##GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
-##use mysql;update user set grant_priv='y' where user='root' and host='%';
+## GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
+## use mysql;update user set grant_priv='y' where user='root' and host='%';
 # mysql5 update password sql
-UPDATE mysql.user SET Password = PASSWORD('AnyWhereis5@0') WHERE User = 'root';
+UPDATE mysql.user SET Password = PASSWORD('$secure_pass') WHERE User = 'root';
 DROP USER ''@'';
 DROP USER ''@'localhost';
 
@@ -33,20 +34,27 @@ EOF
 # force 忽略错误执行
 mysql -u$user -p$pass --force < "secure_mysql.sql"
 
+
+if [ -f "/etc/my.cnf" ]; then
+    file="/etc/my.cnf"
+else
+    file="/etc/mysql/my.cnf"
+fi
+
 # --- secure my.cnf ---
 bind_addr() {
   property="bind_address"
   value="127.0.0.1"
 
   # 检查 my.cnf 文件中是否存在 [mysqld] 属性
-  if grep -q "bind_address" /etc/my.cnf; then
+  if grep -q "bind_address" $file; then
     # 如果存在，使用 sed 命令进行修改
-    sed -i "s/\($property *= *\).*/\1$value/" /etc/my.cnf
+    sed -i "s/\($property *= *\).*/\1$value/" $file
   else
     # 如果不存在，使用 echo 命令添加 [mysqld] 和属性
-    sed -i "s/\(\[mysqld\]\)/\1\n$property=$value/" /etc/my.cnf
+    sed -i "s/\(\[mysqld\]\)/\1\n$property=$value/" $file
   fi
 }
 
-sed -i '/secure_file_priv/d' /etc/my.cnf
+sed -i '/secure_file_priv/d' $file
 bind_addr
