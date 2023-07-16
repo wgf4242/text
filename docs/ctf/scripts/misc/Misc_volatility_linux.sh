9bb9f141296b95c335b98355f90ba26448230cc1
wgf4242 <wgf4242@gmail.com>
2023-07-16 10:47:10 +0800

    sync

--- docs/ctf/scripts/misc/Misc_volatility_linux.sh
+++ docs/ctf/scripts/misc/Misc_volatility_linux.sh
@@ -3,7 +3,16 @@ profile=LinuxUbuntu18_04_6LTS_5_4_0-42-generic_profilex64
 python vol.py -f $file --profile=$profile linux_banner
 python vol.py -f $file --profile=$profile linux_bash >02_bash
 python vol.py -f $file --profile=$profile linux_enumerate_files >01files
-grep '/etc/shadow' 01files | cut -d" " -f 1  | read node
+strings 01files | grep /Documents > 01file_hint1
+strings 01files | grep /home/ > 01file_hint2
+python vol.py -f $file --profile=$profile linux_pslist >pslist
+python vol.py -f $file --profile=$profile linux_pstree  >linux_pstree
+python vol.py -f $file --profile=$profile linux_bash  >linux_bash
+python vol.py -f $file --profile=$profile linux_lsof  >linux_lsof
+python vol.py -f $file --profile=$profile linux_netstat  >linux_netstat
+
+
+node=$(grep '/etc/shadow' 01files | cut -d" " -f 1  )
 python vol.py -f $file --profile=$profile linux_find_file -i $node -O shadow
 # 过滤 /用户名/  , 分析相关文件
 #python vol.py -f $file --profile=$profile linux_find_file -F "/home/bob/Desktop/app.py"
