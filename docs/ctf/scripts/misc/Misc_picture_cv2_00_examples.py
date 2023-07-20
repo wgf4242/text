7ee989de47a03a943429161ee3fa17811cdfc982
wgf4242 <wgf4242@gmail.com>
2023-07-21 07:34:15 +0800

    sync

--- docs/ctf/scripts/misc/Misc_picture_cv2_00_examples.py
+++ docs/ctf/scripts/misc/Misc_picture_cv2_00_examples.py
@@ -2,6 +2,7 @@
 # 先读行再读列
 import cv2
 img =cv2.imread('yusa.png')
+cv_color= cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 cv_color= cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
 cv2.imwrite('flag.png',cv_color)
 
