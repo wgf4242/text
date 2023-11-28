18966739dc86cdcae0a17a343f872fe6e7669363
wgf4242_notebook <wgf4242@gmail.com>
2023-11-28 17:00:00 +0800

    sync

--- docs/ctf/scripts/reverse/reverse_dispython_func.py
+++ docs/ctf/scripts/reverse/reverse_dispython_func.py
@@ -1,3 +1,6 @@
+"""
+FOR_ITER 40 (to 50): 迭代循环的开始，当迭代完成之后将字节码的 counter 加上 40 ，也就是跳转到 50 的位置执行。跳出循环
+"""
 import marshal,dis
 class A():
     def foo():
@@ -17,5 +20,11 @@ def try_test():
     finally:
         c = 3
 # dis.dis(func)
-dis.dis(try_test)
+# dis.dis(try_test)
 # dis.dis(A)
+
+code_to_inspect = """  
+a, b = b, a  
+"""  
+  
+dis.dis(code_to_inspect)  
