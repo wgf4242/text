38bbfce1fffdf43c3ac16f40ebef0e6071b06be7
wgf4242_notebook <wgf4242@gmail.com>
2024-01-01 23:19:21 +0800

    sync

---
+++ docs/ctf/scripts/realworld/sed_ip.sh
@@ -0,0 +1,5 @@
+export newip=192.168.88.2
+export targetip=10.1.1.2
+# cat 1 | sed -r "s/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b/$newip/g"
+sed -r "s/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b/$newip/g" -i *.rc
+sed -r "s/rhosts (\b[0-9]{1,3}\.){3}[0-9]{1,3}\b/rhosts $targetip/g" -i *.rc
