7648ad257d20966700f9649f72677f9c4e141bda
wgf4242 <wgf4242@gmail.com>
2024-01-07 01:26:50 +0800

    sync

--- docs/ctf/scripts/realworld/@sed_ip.sh
+++ docs/ctf/scripts/realworld/@sed_ip.sh
@@ -3,3 +3,4 @@ export targetip=10.1.1.2
 # cat 1 | sed -r "s/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b/$newip/g"
 sed -r "s/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b/$newip/g" -i *.rc
 sed -r "s/rhosts (\b[0-9]{1,3}\.){3}[0-9]{1,3}\b/rhosts $targetip/g" -i *.rc
+sed -r "s/srvhost (\b[0-9]{1,3}\.){3}[0-9]{1,3}\b/srvhost 0.0.0.0/g" -i *.rc
